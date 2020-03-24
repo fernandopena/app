e680264ca913751e9587d3c3fd5dcfd0a0acfb42
Jonathan Tafel <tafelito@gmail.com>
2020-03-23 23:15:10 -0400

    fix background task & handle default map location

---
+++ hooks/use-location.tsx
@@ -0,0 +1,86 @@
+import { useState, useEffect } from 'react';
+import { Platform } from 'react-native';
+import { LocationData } from 'expo-location';
+import * as Location from 'expo-location';
+import * as TaskManager from 'expo-task-manager';
+
+import { locationService } from '../utils/locationService';
+
+const LOCATION_TASK_NAME = 'background-location-task';
+
+if (!TaskManager.isTaskDefined(LOCATION_TASK_NAME)) {
+  TaskManager.defineTask(LOCATION_TASK_NAME, ({ data, error }) => {
+    if (error) {
+      console.log('error', error);
+      // check `error.message` for more details.
+      return;
+    }
+    if (data) {
+      const { locations } = data as any;
+      const [location] = locations;
+      locationService.setLocation(location);
+    }
+  });
+}
+
+export const useLocation = ({
+  runInBackground,
+}: {
+  runInBackground?: Boolean;
+}) => {
+  const [location, setLocation] = useState<LocationData | undefined>();
+  const [error, setError] = useState<string | undefined>();
+
+  useEffect(() => {
+    async function getLocationAsync() {
+      let { status, ios } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setError('El permiso para acceder a la ubicación fue denegado');
+      } else {
+        let location = await Location.getCurrentPositionAsync({});
+        setLocation(location);
+        try {
+          if (
+            runInBackground &&
+            (Platform.OS !== 'ios' ||
+              (Platform.OS === 'ios' && ios.scope === 'always'))
+          ) {
+            await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+              accuracy: Location.Accuracy.Balanced,
+              distanceInterval: 100,
+              //   deferredUpdatesDistance: 100
+              // deferredUpdatesInterval: 10000
+            });
+          }
+        } catch (e) {
+          console.log('getLocationAsync -> e', e);
+          setError('Ups, error al intentar obtener ubicación');
+        }
+      }
+    }
+
+    getLocationAsync();
+
+    return async () => {
+      if (runInBackground) {
+        if (await Location.hasStartedLocationUpdatesAsync(LOCATION_TASK_NAME)) {
+          // console.log('stopLocationUpdatesAsync');
+          await Location.stopLocationUpdatesAsync(LOCATION_TASK_NAME);
+        }
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    function onLocationUpdate(location) {
+      // TODO: save location locally
+    }
+
+    locationService.subscribe(onLocationUpdate);
+    return () => {
+      locationService.unsubscribe(onLocationUpdate);
+    };
+  }, []);
+
+  return { location, error };
+};
--- screens/map/Map.tsx
+++ screens/map/Map.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 import {
   Platform,
   Text,
@@ -6,55 +6,25 @@ import {
   StyleSheet,
   Dimensions,
   TouchableOpacity,
+  SafeAreaView,
 } from 'react-native';
 import { Ionicons as Icon } from '@expo/vector-icons';
-import * as Location from 'expo-location';
-import * as TaskManager from 'expo-task-manager';
-import * as Permissions from 'expo-permissions';
-import MapView, {
-  // PROVIDER_DEFAULT,
-  // PROVIDER_GOOGLE,
-  // MarkerAnimated,
-  AnimatedRegion,
-  Circle,
-  PROVIDER_DEFAULT,
-  // Heatmap,
-  // PROVIDER_GOOGLE,
-  // Heatmap,
-} from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, Heatmap } from 'react-native-maps';
 import BottomSheet from 'reanimated-bottom-sheet';
 
 import { locationService } from '../../utils/locationService';
-import { LocationData } from 'expo-location';
-// import Animated from 'react-native-reanimated';
-// import { getTabBarHeight } from '../components/TabBarComponent';
-import { useSafeArea } from 'react-native-safe-area-context';
+import { useLocation } from '../../hooks/use-location';
+import Colors from '../../constants/Colors';
 
-const LOCATION_TASK_NAME = 'background-location-task';
 const { width, height } = Dimensions.get('window');
 const ASPECT_RATIO = width / height;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
-
-// const OVERLAY_TOP_LEFT_COORDINATE = [35.68184060244454, 139.76531982421875];
-// const OVERLAY_BOTTOM_RIGHT_COORDINATE = [35.679609609368576, 139.76806640625];
-
-// TaskManager.defineTask(LOCATION_TASK_NAME, ({ data, error }) => {
-//   if (error) {
-//     console.log('error', error);
-//     // check `error.message` for more details.
-//     return;
-//   }
-//   if (data) {
-//     const { locations } = data as any;
-//     // console.log("locations", locations);
-//     const { latitude, longitude } = locations[0].coords;
-//     locationService.setLocation({
-//       latitude,
-//       longitude,
-//     });
-//   }
-// });
+// Default location is Argentina's coords
+const DEFAULT_LOCATION = {
+  latitude: -38.00578,
+  longitude: -63.479311,
+};
 
 function PanelContent() {
   return (
@@ -62,7 +32,6 @@ function PanelContent() {
       <View style={{ paddingBottom: 20 }}>
         <View style={{ alignItems: 'center' }}>
           <Text style={panelStyles.panelTitle}>RECOPILANDO INFORMACIÓN</Text>
-          {/* <Icon name="ios-alert" size={50} color="#E50000" /> */}
         </View>
         <Text style={panelStyles.panelSubtitle}>
           La aplicación se irá actualizando con los datos de ubicación y
@@ -148,212 +117,43 @@ const panelStyles = StyleSheet.create({
 });
 
 export default function Map({ navigation }) {
-  // navigation.setOptions({ tabBarVisible: false });
-  const [error, setError] = useState<string | undefined>();
-  const [location, setLocation] = useState<LocationData | undefined>();
-  const [coords, setCoords] = useState<
-    { latitude: number; longitude: number } | undefined
-  >();
-
-  const [coordinate] = useState(new AnimatedRegion());
+  const { location, error } = useLocation({ runInBackground: true });
   const mapRef = useRef<MapView>();
   const refRBSheet = useRef();
 
-  const insets = useSafeArea();
-
-  useEffect(() => {
-    async function getLocationAsync() {
-      let {
-        status,
-        permissions: {
-          location: { ios },
-        },
-      } = await Permissions.askAsync(Permissions.LOCATION);
-      // let { status, ios } = await Location.getPermissionsAsync();
-      // console.log('getLocationAsync -> ios, status', ios, status);
-      if (status !== 'granted') {
-        setError('El permiso para acceder a la ubicación fue denegado');
-      } else if (Platform.OS === 'ios' && ios.scope !== 'always') {
-        setError(
-          'El permiso para acceder a la ubicación en todo momento fue denegado',
-        );
-      } else {
-        try {
-          let location = await Location.getCurrentPositionAsync({});
-          // await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
-          //   accuracy: Location.Accuracy.Balanced,
-          //   // distanceInterval: 50
-          //   // deferredUpdatesDistance: 1000
-          // });
-          setLocation(location);
-          const newCoordinate = {
-            latitude: location.coords.latitude,
-            longitude: location.coords.longitude,
-            // latitude: 40.710065,
-            // longitude: -74.013714,
-            latitudeDelta: LATITUDE_DELTA,
-            longitudeDelta: LONGITUDE_DELTA,
-          };
-          coordinate.timing(newCoordinate).start();
-        } catch (e) {
-          console.log('getLocationAsync -> e', e);
-          setError('Ups, error al intentar obtener ubicación');
-        }
-      }
-    }
-
-    function onLocationUpdate({
-      latitude,
-      longitude,
-    }: {
-      latitude: number;
-      longitude: number;
-    }) {
-      const newCoordinate = {
-        latitude,
-        longitude,
-        latitudeDelta: LATITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA,
-      };
-      setCoords({ latitude, longitude });
-
-      coordinate.timing(newCoordinate).start();
-    }
-
-    getLocationAsync();
-
-    locationService.subscribe(onLocationUpdate);
-    // return async () => {
-    //   // console.log('stopLocationUpdatesAsync');
-    //   // await Location.stopLocationUpdatesAsync(LOCATION_TASK_NAME);
-    // };
-  }, []);
-
-  let text = 'Cargando..';
-  if (error) {
-    text = error;
-  } else if (coords) {
-    text = JSON.stringify(coords);
-  }
-
   return (
     <View style={[styles.container]}>
-      {location && (
-        <MapView
-          ref={mapRef}
-          provider={PROVIDER_DEFAULT}
-          showsUserLocation
-          // followsUserLocation
-          initialRegion={{
-            ...location.coords,
-            // latitude: 40.710065,
-            // longitude: -74.013714,
-            latitudeDelta: LATITUDE_DELTA,
-            longitudeDelta: LONGITUDE_DELTA,
-          }}
-          style={styles.map}
-          showsMyLocationButton={false}
-        >
-          {/* <Circle
-            center={location.coords}
-            radius={700}
-            fillColor="rgba(0, 200, 0, 0.5)"
-            strokeColor="rgba(0, 200, 0, 0.5)"
-            zIndex={2}
-            strokeWidth={1}
-          />
-          <Circle
-            center={{
-              latitude: 40.721264,
-              longitude: -73.99923,
-            }}
-            radius={600}
-            fillColor="#EEC20BAA"
-            strokeColor="#EEC20BAA"
-            // zIndex={2}
-            strokeWidth={1}
-          />
-          <Circle
-            center={{
-              latitude: 40.711631,
-              longitude: -73.99923,
-            }}
-            radius={600}
-            fillColor="#BBCF4CAA"
-            strokeColor="#BBCF4CAA"
-            // zIndex={2}
-            strokeWidth={1}
-          />
-          <Circle
-            center={{
-              latitude: 40.713735,
-              longitude: -73.994234,
-            }}
-            radius={400}
-            fillColor="#EEC20BAA"
-            strokeColor="#EEC20BAA"
-            // zIndex={2}
-            strokeWidth={1}
-          />
-          <Circle
-            center={{
-              latitude: 40.729301,
-              longitude: -73.996745,
-            }}
-            radius={700}
-            fillColor="rgba(255, 142, 0, 0.5)"
-            strokeColor="rgba(255, 142, 0, 0.5)"
-            // zIndex={2}
-            strokeWidth={1}
-          /> */}
-          {/* <Heatmap
-            points={[
-              {
-                latitude: location.coords.latitude,
-                longitude: location.coords.longitude,
-                weight: 50,
-              },
-              {
-                latitude: -34.612146,
-                longitude: -58.384734,
-                weight: 80,
-              },
-              {
-                latitude: -34.609858,
-                longitude: -58.384788,
-                weight: 100,
-              },
-              {
-                latitude: -34.604476,
-                longitude: -58.374188,
-                weight: 60,
-              },
-              // {
-              //   latitude: 40.729301,
-              //   longitude: -73.996745,
-              //   weight: 90,
-              // },
-            ]}
-            radius={Platform.OS === 'ios' ? 150 : 50}
-            // opacity={0.7}
-            gradient={{
-              colors: ['#79BC6A', '#BBCF4C', '#EEC20B', '#F29305', '#E50000'],
-              startPoints: [0.01, 0.25, 0.5, 0.75, 1],
-              colorMapSize: 100,
-            }}
-          /> */}
-          {/* <MarkerAnimated coordinate={coordinate} /> */}
-        </MapView>
-      )}
-      <View style={[styles.buttonContainer, { paddingTop: insets.top }]}>
+      <MapView
+        ref={mapRef}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation
+        loadingEnabled
+        initialRegion={
+          location
+            ? {
+                ...location.coords,
+                latitudeDelta: LATITUDE_DELTA,
+                longitudeDelta: LONGITUDE_DELTA,
+              }
+            : undefined
+        }
+        initialCamera={{
+          center: DEFAULT_LOCATION,
+          pitch: 1,
+          heading: 1,
+          altitude: 11,
+          zoom: 4,
+        }}
+        style={styles.map}
+        showsMyLocationButton={false}
+      />
+      <SafeAreaView style={[styles.buttonContainer]}>
         <TouchableOpacity
           activeOpacity={0.8}
           style={[styles.button, styles.locationButton]}
-          // onPress={() => navigation.navigate('MapInfo')}
           onPress={() => navigation.navigate('Help')}
         >
           <Icon
-            // name="md-information-circle-outline"
             name={`${Platform.OS === 'ios' ? 'ios' : 'md'}-help-circle-outline`}
             size={24}
             color="rgba(66,135,244,1)"
@@ -362,6 +162,7 @@ export default function Map({ navigation }) {
         <TouchableOpacity
           activeOpacity={0.8}
           style={[styles.button, styles.infoButton]}
+          disabled={!location}
           onPress={() =>
             mapRef.current.animateToRegion({
               ...location.coords,
@@ -373,19 +174,17 @@ export default function Map({ navigation }) {
           <Icon
             name={`${Platform.OS === 'ios' ? 'ios' : 'md'}-locate`}
             size={24}
-            color="rgba(66,135,244,1)"
+            color={!location ? Colors.tabIconDefault : rgba(66, 135, 244, 1)}
           />
         </TouchableOpacity>
-      </View>
-      {location && (
-        <BottomSheet
-          ref={refRBSheet}
-          snapPoints={['40%', 150, 50]}
-          renderContent={PanelContent}
-          renderHeader={PanelHeader}
-          initialSnap={1}
-        />
-      )}
+      </SafeAreaView>
+      <BottomSheet
+        ref={refRBSheet}
+        snapPoints={['40%', 150, 50]}
+        renderContent={PanelContent}
+        renderHeader={PanelHeader}
+        initialSnap={1}
+      />
     </View>
   );
 }
