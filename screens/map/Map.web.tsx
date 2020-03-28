2439f33c5dcad24fd024d53a0b91aff93da8af62
Fernando Pena <pena.fernan@gmail.com>
2020-03-28 09:16:38 -0300

    Loading heatmap data from the server. Restructured Map related files.

--- screens/map/Map.web.tsx
+++ screens/map/Map.web.tsx
@@ -1,54 +1,64 @@
 // @ts-nocheck
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View } from 'react-native';
 import GoogleMapReact from 'google-map-react';
+import { getHeatmapData } from '../../api/services';
 
 import { useLocation } from '../../hooks/use-location';
 
-// const heatmapData = {
-//   positions: [
-//     {
-//       lat: -34.612146,
-//       lng: -58.384734,
-//       weight: 80,
-//     },
-//     {
-//       lat: -34.609858,
-//       lng: -58.384788,
-//       weight: 100,
-//     },
-//     {
-//       lat: -34.604476,
-//       lng: -58.374188,
-//       weight: 60,
-//     },
-//   ],
-//   options: {
-//     radius: 20,
-//     opacity: 0.7,
-//   },
-// };
+import { 
+  shouldUpdateHeatMap, 
+  HEATMAP_WEB_ZOOM, 
+  HEATMAP_WEB_RADIUS, 
+  HEATMAP_WEB_OPACITY, 
+  HEATMAP_GET_DATA_DISTANCE,
+  DEFAULT_LOCATION_WEB
+ } from './mapConfig';
 
-const DEFAULT_LOCATION = {
-  center: {
-    lat: -38.00578,
-    lng: -63.479311,
-  },
-  zoom: 5,
-};
 
 export default function Map({ navigation }) {
+  const [heatmapData, setHeatmapData] = useState({});
   const { location } = useLocation();
-
   const coords = location
     ? {
         center: {
           lat: location.coords.latitude,
           lng: location.coords.longitude,
         },
-        zoom: 15,
+        zoom: HEATMAP_WEB_ZOOM,
       }
-    : DEFAULT_LOCATION;
+    : DEFAULT_LOCATION_WEB;
+
+
+  useEffect(() => {
+    if (location) {
+      const locationCoords = {lat: location.coords.latitude, lng: location.coords.longitude};
+      if (shouldUpdateHeatMap(heatmapData, locationCoords)) {
+        getHeatmapData({
+          ...locationCoords,
+          distance: HEATMAP_GET_DATA_DISTANCE
+        }).then((response)=>{
+          const positions = response.data;
+          const mapData = {
+            positions: positions,
+            options: {
+              radius: HEATMAP_WEB_RADIUS,
+              opacity: HEATMAP_WEB_OPACITY,
+            },
+          };
+          const now = new Date().getTime();
+          const heatmapData = {
+            mapData: mapData,
+            lastUpdated: now,
+            center: locationCoords
+          };
+          setHeatmapData(heatmapData);
+        }).catch((error) => {
+          console.log(error);
+        });
+      }
+    }
+  }, [location]);
 
   return (
     <View style={{ height: '100vh', width: '100%' }}>
@@ -60,8 +70,8 @@ export default function Map({ navigation }) {
         // defaultZoom={coords.zoom}
         center={coords.center}
         zoom={coords.zoom}
-        heatmapLibrary
-        // heatmap={heatmapData}
+        heatmapLibrary={true}
+        heatmap={heatmapData.mapData}
         options={{ fullscreenControl: false, zoomControl: false }}
       />
     </View>
