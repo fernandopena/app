a6bdd9722396faec317b09afc56ecd5bc8a3bf82
Bryan Tafel <bryantafel@macbook-pro.lan>
2020-03-28 07:28:49 -0400

    add uuid header

--- api/axiosInstance.ts
+++ api/axiosInstance.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import getEnvVars from './environment';
+import Constants from 'expo-constants';
 
 const { apiUrl } = getEnvVars();
 
 export const axiosInstance = axios.create({
   baseURL: apiUrl,
+  headers: { 'X-CO-UUID': Constants.installationId },
 });
